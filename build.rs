c0150217a13f804e0549752eb8b0a2b7daf1822e
thesimplekid <tsk@thesimplekid.com>
2023-02-28 01:18:54 -0500

    tonic build

--- build.rs
+++ build.rs
@@ -1,10 +1,6 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    /*
-        tonic_build::configure()
-        .build_server(false)
+    tonic_build::configure()
         .protoc_arg("--experimental_allow_proto3_optional")
-       .compile(&["./proto/nauthz.proto"], &["../../proto"])?;
-    */
-    tonic_build::compile_protos("./proto/nauthz.proto")?;
+        .compile(&["./proto/nauthz.proto"], &["../../proto"])?;
     Ok(())
 }
